af086f0a339461b8b21c6043ae2537e1c1d47636
mobile <mobile@jangjaeman-ui-MacBook-Pro.local>
2019-04-19 14:13:40 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,50 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Customer from './components/Customer';
 import './App.css';
 
+const customers = [
+  {
+    'id' : 1,
+    'image' : 'https://placeimg.com/64/64/1',
+    'name': 'aaa',
+    'birthday': '111',
+    'gender': 'bbb',
+    'job': 'ccc'
+  },
+  {
+    'id' : 2,
+    'image' : 'https://placeimg.com/64/64/2',
+    'name': 'aaa',
+    'birthday': '111',
+    'gender': 'bbb',
+    'job': 'ccc'
+  },{
+    'id' : 3,
+    'image' : 'https://placeimg.com/64/64/3',
+    'name': 'aaa',
+    'birthday': '111',
+    'gender': 'bbb',
+    'job': 'ccc'
+  }
+]
 class App extends Component {
   render() {
     return (
-      <div className="gray-background">
-        <img src= {logo} alt = "logo" />
-        <h2>Lets's develop Management System</h2>
+      <div>
+        { customers.map( c => {
+            return (
+              <Customer
+                key = {c.id}
+                id = {c.id}
+                name = {c.name}
+                image = {c.image}
+                birthday = {c.birthday}
+                gender = {c.gender}
+                job = {c.job}
+              />
+            )
+          })
+        }
       </div>
     );
   }
