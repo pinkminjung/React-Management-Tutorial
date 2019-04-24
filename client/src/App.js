17cc432d69d0adcc71bb38186c73e504513c22f7
mobile <mobile@jangjaeman-ui-MacBook-Pro.local>
2019-04-24 11:19:35 +0900

    Add Modal & CustomerDelete Component

--- client/src/App.js
+++ client/src/App.js
@@ -11,9 +11,16 @@ import TableBody from '@material-ui/core/TableBody';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import { withStyles } from '@material-ui/core/styles';
 
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
 const styles = theme => ({
   root: {
     width: '100%',
+    minWidth: '1080',
     marginTop: theme.spacing.unit * 3,
     overflowX: "auto"
   },
@@ -22,7 +29,70 @@ const styles = theme => ({
   },
   progress: {
     margin: theme.spacing.unit * 2
-  }
+  },
+  grow: {
+    flexGrow: 1,
+  },
+  tableHead : {
+    fontSize: '1.0rem'
+  },
+  menu: {
+    marginTop: 15,
+    marginBottom: 15,
+    display: 'flex',
+    justifyContent: 'center'
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
+  title: {
+    display: 'none',
+    [theme.breakpoints.up('sm')]: {
+      display: 'block',
+    },
+  },
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing.unit,
+      width: 'auto',
+    },
+  },
+  searchIcon: {
+    width: theme.spacing.unit * 9,
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputRoot: {
+    color: 'inherit',
+    width: '100%',
+  },
+  inputInput: {
+    paddingTop: theme.spacing.unit,
+    paddingRight: theme.spacing.unit,
+    paddingBottom: theme.spacing.unit,
+    paddingLeft: theme.spacing.unit * 10,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      width: 120,
+      '&:focus': {
+        width: 200,
+      },
+    },
+  },
 });
 class App extends Component {
   // state = {
@@ -34,14 +104,16 @@ class App extends Component {
     super(props);
     this.state = {
       customers: '',
-      completed: 0
+      completed: 0,
+      searchKeyword: ''
     }
   }
 
   stateRefresh = () => {
     this.setState({
       customers: '',
-      completed: 0
+      completed: 0,
+      searchKeyword: ''
     });
     this.callApi()
       .then(res => this.setState({customers: res}))
@@ -65,20 +137,62 @@ class App extends Component {
     const { completed } = this.state;
     this.setState( { completed: completed >= 100 ? 0 : completed + 1});
   }
+
+  handleValueChange = (e) => {
+    let nextState = {};
+    nextState[e.target.name] = e.target.value;
+    this.setState(nextState);
+  }
   render() {
+    const filterComponents = (data) => {
+      data = data.fileter((c) => {
+        return c.name.indexOf(this.state.searchKeyword) > -1;
+      });
+      return data.map((c) => {
+        return <filterComponents />
+      });
+    }
     const { classes } = this.props;
+    const cellList = [ "번호", "프로플 이미지", "이름", "생년월일", "성별", "직업"];
     return (
-      <div>
-        <Paper className = {classes.root}>
+      <div className = {classes.root}>
+        <AppBar position="static">
+        <Toolbar>
+          <IconButton className={classes.menuButton} color="inherit" aria-label="Open drawer">
+            <MenuIcon />
+          </IconButton>
+          <Typography className={classes.title} variant="h6" color="inherit" noWrap>
+            고객 관리 시스템
+          </Typography>
+          <div className={classes.grow} />
+          <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon />
+            </div>
+            <InputBase
+              placeholder="검색하기"
+              classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput,
+              }}
+              name = "searchKeyword" 
+              value = { this.state.searchKeyword}
+              onChange = { this.handleValueChange}
+            />
+          </div>
+        </Toolbar>
+      </AppBar>
+      <div className={classes.menu}>
+        <CustomerAdd stateRefresh = { this.stateRefresh } />
+      </div>
+        <Paper >
           <Table>
             <TableHead>
-              <TableCell>번호</TableCell>
-              <TableCell>이미지</TableCell>
-              <TableCell>이름</TableCell>
-              <TableCell>생년월일</TableCell>
-              <TableCell>성별</TableCell>
-              <TableCell>직업</TableCell>
-              <TableCell>설정</TableCell>
+              <TableRow>
+                {cellList.map( c => {
+                  return <TableCell className = { classes.tableHead }>{c}</TableCell>
+                })}
+              </TableRow>
             </TableHead>
             <TableBody>
             { 
@@ -105,7 +219,6 @@ class App extends Component {
             </TableBody>
           </Table>
         </Paper>
-        <CustomerAdd stateRefresh = { this.stateRefresh } />
       </div>
     );
   }
